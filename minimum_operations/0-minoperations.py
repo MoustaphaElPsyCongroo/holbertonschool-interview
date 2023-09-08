4299d1450aaebdc17a3faa9f5b4079a41dd1fa9c
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-09-08 19:07:49 +0200

    Minimum operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) > 1:
+    n = argv[1]
+
+
+def minOperations(n):
+    numOperations = 0
+    return numOperations
+
+
+if __name__ == '__main__':
+    minOperations(n)
