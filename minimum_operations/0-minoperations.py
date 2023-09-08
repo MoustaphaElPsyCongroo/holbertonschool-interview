e9cae7a61067e6865da74ddc45cada25627e7f5f
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-09-08 20:04:07 +0200

    Minimum operations: complete

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,15 +3,29 @@
 only two operations in this file: Copy All and Paste. Given a number n, write a
 method that calculates the fewest number of operations needed to result in
 exactly n H characters in the file."""
-from sys import argv
-if len(argv) > 1:
-    n = argv[1]
+from math import sqrt
 
 
 def minOperations(n):
-    numOperations = 0
-    return numOperations
+    number_operations = 0
+    while n != 1:
+        n, divider_operations = calc_highest_divider(n)
+        number_operations += divider_operations
+    return number_operations
 
 
-if __name__ == '__main__':
-    minOperations(n)
+def calc_highest_divider(num):
+    """Find the highest divider of a number
+
+        Return: The number of operations to obtain this divider
+    """
+    num_operations = num
+    highest_divider = 1
+    if num < 0:
+        return (1, 0)
+    for i in range(2, round(sqrt(num)) + 1):
+        if num % i == 0:
+            highest_divider = num // i
+            num_operations = i
+            break
+    return (highest_divider, num_operations)
