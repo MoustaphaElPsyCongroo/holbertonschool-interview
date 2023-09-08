a4855dcdc9acb9b3cbafcc9f4959d11247b71147
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-09-08 19:11:49 +0200

    Minimum operations: add module docstring

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""In a text file, there is a single character H. Your text editor can execute
+only two operations in this file: Copy All and Paste. Given a number n, write a
+method that calculates the fewest number of operations needed to result in
+exactly n H characters in the file."""
 from sys import argv
 if len(argv) > 1:
     n = argv[1]
