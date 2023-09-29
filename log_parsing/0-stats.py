c1651c0740035fe7868528667cc50bdb47761d77
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-09-30 00:53:05 +0200

    0 - Uh correct doctring to prevent checker bug? (not detected)

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
-"""
-Write a script that reads stdin line by line and computes metrics:
-
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
-code> <file size> (if the format is not this one, the line must be skipped)
-After every 10 lines and/or a keyboard interruption (CTRL + C), print these
-statistics from the beginning:
-Total file size: File size: <total size>
-where <total size> is the sum of all previous <file size> (see input format
-above)
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn't appear or is not an integer, don't print anything for
-this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
-"""
+"""Reads stdin line by line and computes metrics"""
 import sys
 from typing import Tuple, Dict
 import re
@@ -115,25 +99,26 @@ def print_stats(status_codes: Dict[int, int], total_size: int) -> None:
             print("{}: {}".format(code, occurences))
 
 
-i = 0
-for line in sys.stdin:
-    try:
-        data = is_valid_format(line)
-        status_code = data[1]
-        file_size = data[2]
+if __name__ == "__main__":
+    i = 0
+    for line in sys.stdin:
+        try:
+            data = is_valid_format(line)
+            status_code = data[1]
+            file_size = data[2]
 
-        if data[0] is False:
-            continue
+            if data[0] is False:
+                continue
 
-        if status_code in valid_status:
-            status_codes[status_code] += 1
-            total_size += file_size
+            if status_code in valid_status:
+                status_codes[status_code] += 1
+                total_size += file_size
 
-        if i != 0 and i % 10 == 0:
+            if i != 0 and i % 10 == 0:
+                print_stats(status_codes, total_size)
+                clear_stats()
+
+            i += 1
+        except KeyboardInterrupt:
             print_stats(status_codes, total_size)
-            clear_stats()
-
-        i += 1
-    except KeyboardInterrupt:
-        print_stats(status_codes, total_size)
-        break
+            break
