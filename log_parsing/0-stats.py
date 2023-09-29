b624ff49160ca7d5f32f8bd8f4108710e343cf17
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-09-30 01:06:29 +0200

    Thanks checker

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-
 """
-Reads stdin line by line and computes metrics
+Write a script that reads stdin line by line and computes metrics:
+
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+code> <file size> (if the format is not this one, the line must be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+statistics from the beginning:
+Total file size: File size: <total size>
+where <total size> is the sum of all previous <file size> (see input format
+above)
+Number of lines by status code:
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+if a status code doesn’t appear or is not an integer, don’t print anything for
+this status code
+format: <status code>: <number>
+status codes should be printed in ascending order
 """
 
 
@@ -77,19 +90,19 @@ def is_valid_format(line: str) -> Tuple[bool, int, int]:
     return (True, int(status_code), int(file_size))
 
 
-# def clear_stats():
-#     """Restores all stats to default (0)"""
-#     globals()["status_codes"] = {
-#         200: 0,
-#         301: 0,
-#         400: 0,
-#         401: 0,
-#         403: 0,
-#         404: 0,
-#         405: 0,
-#         500: 0
-#     }
-#     globals()["total_size"] = 0
+def clear_stats():
+    """Restores all stats to default (0)"""
+    globals()["status_codes"] = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
+    globals()["total_size"] = 0
 
 
 def print_stats(status_codes: Dict[int, int], total_size: int) -> None:
@@ -121,17 +134,7 @@ if __name__ == "__main__":
 
             if i != 0 and i % 10 == 0:
                 print_stats(status_codes, total_size)
-                status_codes = {
-                    200: 0,
-                    301: 0,
-                    400: 0,
-                    401: 0,
-                    403: 0,
-                    404: 0,
-                    405: 0,
-                    500: 0
-                }
-                total_size = 0
+                clear_stats()
 
             i += 1
         except KeyboardInterrupt:
