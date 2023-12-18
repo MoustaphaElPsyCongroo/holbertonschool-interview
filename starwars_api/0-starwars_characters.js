3361bc4c4ff9557cdd56f239f5e805efae279f33
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-12-18 18:23:29 +0100

    Rain

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,44 +1,25 @@
 #!/usr/bin/node
+
 const request = require('request');
-const args = process.argv;
+const arg = process.argv[2];
+const url = (`https://swapi-api.hbtn.io/api/films/${arg}`);
 
-const getChar = () => {
-  return new Promise((resolve, reject) => {
-    request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const data = JSON.parse(body);
-        resolve(data.characters);
-      }
-    });
+const charNames = (characters, i = 0) => {
+  if (i === characters.length) return;
+  request(characters[i], (error, response, body) => {
+    if (error) throw error;
+    /* convert a string of characters JSON to a javascript object and print it */
+    console.log(JSON.parse(body).name);
+    /* call recursively the function charnames and increment it to pass at the next character */
+    charNames(characters, i + 1);
   });
 };
 
-const getName = (url) => {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const data = JSON.parse(body);
-        console.log(data.name);
-        resolve(data.name);
-      }
-    });
-  });
-};
-
-const printNames = async () => {
-  try {
-    const arrName = await getChar();
-
-    for (const element of arrName) {
-      await getName(element);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-printNames();
+/* request to the API to recover movie informations */
+request(url, function (error, response, body) {
+  if (error) throw error;
+  /* extract url of characters from the body json response and store it in a var char */
+  const char = JSON.parse(body).characters;
+  /* call the function charNames to print the characters */
+  charNames(char);
+});
